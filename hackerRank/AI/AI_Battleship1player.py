0b59092a8cc9b5bef633837d7252d861c37b79ac
Admin <etienne1210@gmail.com>
2019-11-10 12:38:57 +0100

    Initial commit gathering all my old repo

---
+++ hackerRank/AI/AI_Battleship1player.py
@@ -0,0 +1,152 @@
+import random,math
+
+N = int(input())
+grid=[]
+for _ in range(N):
+    grid.append(input())
+
+def find_hyphens():
+    res=[]
+    for i in range(N):
+        for j in range(N):
+            if grid[i][j]=='-':
+                res.append([i,j])
+    return res
+
+def find_h():
+    res=[]
+    count=0
+    for i in range(N):
+        for j in range(N):
+            if grid[i][j]=='h':
+                count+=1
+                res.append([i,j])
+    return count,res
+
+def dfs_x(grid, start):
+    visited, stack = set(), [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            try :
+                if grid[vertex[0]][vertex[1]+1]=='-' and vertex[1]+1<10:
+                    stack.extend([(vertex[0],vertex[1]+1)])
+            except:
+                pass
+            try :
+                if grid[vertex[0]][vertex[1]-1]=='-' and vertex[1]-1>=0:
+                    stack.extend([(vertex[0],vertex[1]-1)])
+            except:
+                pass
+    return visited
+
+def dfs_y(grid, start):
+    visited, stack = set(), [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            try :
+                if grid[vertex[0]+1][vertex[1]]=='-' and vertex[0]+1<10:
+                    stack.extend([(vertex[0]+1,vertex[1])])
+            except:
+                pass
+            try :
+                if grid[vertex[0]-1][vertex[1]]=='-' and vertex[0]-1>=0:
+                    stack.extend([(vertex[0]-1,vertex[1])])
+            except:
+                pass
+    return visited
+
+c,l=find_h()
+p=False
+if c==1 :
+    i,j=l[0]
+    try:
+        if grid[i-1][j]=='-' and p==False:
+            print(i-1,j)
+            p=True
+    except:
+        pass
+    try:
+        if grid[i+1][j]=='-' and p==False:
+            print(i+1,j)
+            p=True
+    except:
+        pass
+    try:
+        if grid[i][j-1]=='-' and p==False:
+            print(i,j-1)
+            p=True
+    except:
+        pass
+    try:
+        if grid[i][j+1]=='-' and p==False:
+            print(i,j+1)
+            p=True
+    except:
+        pass
+
+elif c>1:
+    seti,setj=[set(),set()]
+    boatInLign=False
+    val=0
+    for v in l:
+        seti.add(v[0])
+        setj.add(v[1])
+    if len(seti)<len(setj):
+        boatInLign=True
+        val=list(seti)[0]
+    else:
+        val=list(setj)[0]
+    
+    if boatInLign:
+        valj=[v[1] for v in l]
+        minj=min(valj)
+        maxj=max(valj)
+        try:
+            if grid[val][minj-1]=='-' and p==False:
+                print(val,minj-1)
+                p=True
+        except:
+            pass
+        try:
+            if grid[val][maxj+1]=='-' and p==False:
+                print(val,maxj+1)
+                p=True
+        except:
+            pass
+    else:
+        vali=[v[0] for v in l]
+        mini=min(vali)
+        maxi=max(vali)
+        try:
+            if grid[mini-1][val]=='-' and p==False:
+                print(mini-1,val)
+                p=True
+        except:
+            pass
+        try:
+            if grid[maxi+1][val]=='-' and p==False:
+                print(maxi+1,val)
+                p=True
+        except:
+            pass
+
+
+else:
+    hyphens = find_hyphens()
+    count = len(hyphens)
+    if count>85:
+        print(' '.join([str(v) for v in random.choice(hyphens)]))
+    else:
+        max_long_x_y=0
+        max_x,max_y=[0,0]
+        for v in hyphens:
+            long_x=len(dfs_x(grid, (v[0],v[1])))
+            long_y=len(dfs_y(grid, (v[0],v[1])))
+            if long_x+long_y>max_long_x_y:
+                max_long_x_y=long_x+long_y
+                max_x,max_y=[v[0],v[1]]
+        print(max_x,max_y)
---
+++ hackerRank/graph/BFS_ShortestReach.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import sys
+from collections import defaultdict
+
+def bfs(n, m, graph, start, w):
+    distances = dict()
+    for i in range(1,n+1) : distances[i]=-1
+    distances[start]=0
+    queue = [start]
+    while queue:
+        node = queue.pop(0)
+        for neighbor in graph[node]:
+            if distances[neighbor]==-1:
+                distances[neighbor]=distances[node]+w
+                queue.append(neighbor)    
+    del distances[start]
+    return distances.values()
+    
+
+if __name__ == "__main__":
+    q = int(input().strip())
+    for a0 in range(q):
+        n, m = input().strip().split(' ')
+        n, m = [int(n), int(m)]
+        graph = defaultdict(set)
+        for edges_i in range(m):
+            edges_t = [int(edges_temp) for edges_temp in input().strip().split(' ')]
+            graph[edges_t[0]].add(edges_t[1])
+            graph[edges_t[1]].add(edges_t[0])
+        s = int(input().strip())
+        result = bfs(n, m, graph, s, 6)
+        print (" ".join(map(str, result)))
